b6d7316b3e259ade53ca91f7b57cf5f8750a06fc
alexkat20 <alexkat20@rambler.ru>
2022-08-23 19:06:19 +0300

    Added file for tasks

---
+++ tests/test_maximum_number.py
@@ -0,0 +1,9 @@
+import unittest
+from Task1 import MaxNumbers
+
+
+class TestMaxNumber(unittest.TestCase):
+    def check_max_number(self):
+        test_numbers = [1, 2, 3, 4, 5, 6]
+        max_numbers = MaxNumbers(test_numbers)
+        self.assertEqual(max(test_numbers), max_numbers.find_maximum_number())
