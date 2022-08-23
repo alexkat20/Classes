ddee7bee8b72a637149365e4290b99143cd778bb
alexkat20 <alexkat20@rambler.ru>
2022-08-23 19:06:14 +0300

    Added file for tasks

---
+++ Task1.py
@@ -0,0 +1,13 @@
+"""
+Write a function to find the maximum number without using max() function
+
+"""
+
+
+class MaxNumbers:
+    def __init__(self, *args):
+        self.numbers = args
+
+    def find_maximum_number(self):
+        pass
+
