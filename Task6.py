05eeb87df555b361916099e6381cacc8bc34132c
alexkat20 <alexkat20@rambler.ru>
2022-12-24 00:49:50 +0200

    Solved Task number 6 in file Task6.py

--- Task6.py
+++ Task6.py
@@ -8,3 +8,12 @@
 
 x1, y1 = int(input()), int(input())
 x2, y2 = int(input()), int(input())
+
+if x1 == x2 and (abs(y1 - y2) == 1 or y1 == y2):
+    print("YES")
+elif ((x1 - x2) == 1) and (abs(y1 - y2) == 1 or y1 == y2):
+    print("YES")
+elif ((x2 - x1) == 1) and (abs(y1 - y2) == 1 or y1 == y2):
+    print("YES")
+else:
+    print("NO")
